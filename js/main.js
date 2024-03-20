c83bead055f7c9bd5d830fca30f0832afb6e6d46
Carlos Carneglia <cd.carneglia@gmail.com>
2024-03-20 11:20:29 -0300

    img broken

--- js/main.js
+++ js/main.js
@@ -4,13 +4,12 @@ let pass = document.getElementById("password");
 function changeValue(){
     if (pass.type === "password"){
         pass.type = "text";
-        eyeEl.src = "../recourses/images/eye.png"
+        eyeEl.src = "./recourses/images/eye.png"
         eyeEl.alt = "Imagen de un Ojo, que presionando en él, encripta la contraseña";  
-        console.log(eyeEl.alt)
      
     } else{
         pass.type = "password"
-        eyeEl.src = "../recourses/images/hide.png"
+        eyeEl.src = "./recourses/images/hide.png"
     }
 }
 
