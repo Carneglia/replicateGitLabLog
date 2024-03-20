2b0874223aab3404449e1d1bcff5e8d81750bb98
Carlos Carneglia <cd.carneglia@gmail.com>
2024-03-20 00:16:39 -0300

    function show and hide password added

---
+++ js/main.js
@@ -0,0 +1,15 @@
+let eyeEl = document.getElementById("eye-el");
+let pass = document.getElementById("password");
+
+function changeValue(){
+    if (pass.type === "password"){
+        pass.type = "text";
+        eyeEl.src = "../recourses/images/eye.png"
+     
+    } else{
+        pass.type = "password"
+        eyeEl.src = "../recourses/images/hide.png"
+    }
+}
+
+
